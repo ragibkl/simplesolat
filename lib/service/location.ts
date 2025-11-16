25ab8237a12ed3a5073fd7fb65c97bf9d50bb381
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2025-11-16 18:39:34 +0800

    request permission in alert dialog

--- lib/service/location.ts
+++ lib/service/location.ts
@@ -5,7 +5,23 @@ async function getLocationPermissions(): Promise<boolean> {
   try {
     const fgPermissionCurrent = await Location.getForegroundPermissionsAsync();
     if (fgPermissionCurrent.status !== "granted") {
-      const fgPermission = await Location.requestForegroundPermissionsAsync();
+      const fgPermission: Location.LocationPermissionResponse = await new Promise((resolve, _reject) => {
+          Alert.alert(
+            "Location Permission",
+            "This app needs your location to determine which of the JAKIM prayer zones you're in, so it can display accurate prayer times for your area.",
+            [
+              {
+                text: "Ok",
+                style: "default",
+                onPress: async () => {
+                  const permission =
+                    await Location.requestForegroundPermissionsAsync();
+                  resolve(permission);
+                },
+              },
+            ],
+          );
+        });
       if (fgPermission.status !== "granted") {
         return false;
       }
@@ -17,7 +33,7 @@ async function getLocationPermissions(): Promise<boolean> {
         await new Promise((resolve, _reject) => {
           Alert.alert(
             "Background Location Permission",
-            "This app needs access to your location in background in order to keep your Waktu Solat Zone up to date. Please allow permission in the app settings page",
+            "This app needs background location access to keep your prayer times widget updated with the correct JAKIM prayer zone as you move around Malaysia, even when the app is not open.",
             [
               {
                 text: "Ok",
