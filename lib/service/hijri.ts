0e55a44e227093f1cd6aadf6595d97d6e8121697
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2026-03-19 14:33:44 +0800

    use mabimbs calculator

--- lib/service/hijri.ts
+++ lib/service/hijri.ts
@@ -1,25 +1,12 @@
-import { gregorianToHijri } from "@tabby_ai/hijri-converter";
-
-const HIJRI_MONTHS = [
-  "Muharram",
-  "Safar",
-  "Rabi al-Awwal",
-  "Rabi al-Thani",
-  "Jumada al-Ula",
-  "Jumada al-Thani",
-  "Rajab",
-  "Shaaban",
-  "Ramadan",
-  "Shawwal",
-  "Dhul Qaadah",
-  "Dhul Hijjah",
-];
+import { gregorianToHijri } from "islamic-date";
 
 export function getHijriDateText(date: Date): string {
-  const hijri = gregorianToHijri({
-    year: date.getFullYear(),
-    month: date.getMonth() + 1,
-    day: date.getDate(),
-  });
-  return `${hijri.day} ${HIJRI_MONTHS[hijri.month - 1]} ${hijri.year}`;
+  const hijri = gregorianToHijri(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate(),
+    "en",
+    "mabims",
+  );
+  return hijri.fullDate;
 }
